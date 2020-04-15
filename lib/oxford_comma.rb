e1d3c2eb25776323100e09a57ce9eedc7d3cd806
Dan Ste. Marie Jr <dste.mariejr@gmail.com>
2020-04-15 02:26:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(fruit)
-  if(fruit.size == 1)
+  if(fruit.size == 2)
     return fruit.join(" and ")
-  elsif (fruit.size == 2)
-    return fruit.join(" , and")
+  elsif (fruit.size == 3)
+    return fruit.join(" , ")
   else
     return fruit
   end
